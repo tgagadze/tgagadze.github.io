ea39a3a7674502f82a8a046c5a7ff9929557d747
Tornike Gagadze <t.gagadze@owl.local>
2019-07-02 14:27:42 +0400

    new version

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.873840397b902ab8eabd102c6bd5d251.js"
+  "/precache-manifest.1e8a6eedec70954e2270efd975456e10.js"
 );
 
 self.addEventListener('message', (event) => {
