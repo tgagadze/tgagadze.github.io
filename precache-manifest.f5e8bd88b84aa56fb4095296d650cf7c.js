f63e4ffee9433c0116104c30221ba164b6e97e7f
Tornike Gagadze <t.gagadze@owl.local>
2019-07-09 15:30:37 +0400

    update

---
+++ precache-manifest.f5e8bd88b84aa56fb4095296d650cf7c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543940ecb5ee7702d9cf926a10546059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ff3d5bdbe78a0ab557a",
+    "url": "/static/css/main.7eb6abcb.chunk.css"
+  },
+  {
+    "revision": "848b904a655fb8e91eb8",
+    "url": "/static/js/2.d31e1918.chunk.js"
+  },
+  {
+    "revision": "3ff3d5bdbe78a0ab557a",
+    "url": "/static/js/main.d570a60b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c136c78fb5074b3434bb8a2e5e587d30",
+    "url": "/static/media/20.c136c78f.png"
+  },
+  {
+    "revision": "0ba3afee4ca77cfd0ef1757fbec15ef1",
+    "url": "/static/media/arial_geo.0ba3afee.ttf"
+  },
+  {
+    "revision": "af06ce25b3d4317bb1a2ce8d500f1c9c",
+    "url": "/static/media/bottle.af06ce25.png"
+  },
+  {
+    "revision": "b518e52419b800262f7d4d8cfdf8b4ea",
+    "url": "/static/media/box1.b518e524.png"
+  },
+  {
+    "revision": "11a232ef347283fd7595df8cb58adfd1",
+    "url": "/static/media/box2.11a232ef.png"
+  },
+  {
+    "revision": "dbe76a520ae12de26cc563de9f998300",
+    "url": "/static/media/box3.dbe76a52.png"
+  },
+  {
+    "revision": "4e502984db4b00d845e133b44d591a25",
+    "url": "/static/media/bpg_mrgvlovani_caps_2010.4e502984.ttf"
+  },
+  {
+    "revision": "4e8525b024db9b2bda5b97a4bd91efa8",
+    "url": "/static/media/bpg_nino_mtavruli_normal.4e8525b0.ttf"
+  },
+  {
+    "revision": "724c3977c19c1cbefc39c05cb4e4940c",
+    "url": "/static/media/break.724c3977.mp3"
+  },
+  {
+    "revision": "0686d897d8f9861271689c9fbfa2b555",
+    "url": "/static/media/drop.0686d897.mp3"
+  },
+  {
+    "revision": "e24cdde642c31d46f36f7d4a82b590af",
+    "url": "/static/media/free.e24cdde6.png"
+  },
+  {
+    "revision": "1622bae2d34c7fae8f0056711ed348a1",
+    "url": "/static/media/game-background.1622bae2.jpg"
+  },
+  {
+    "revision": "424a31695bb06e1e4a870f711e3f5bc9",
+    "url": "/static/media/gg.424a3169.png"
+  },
+  {
+    "revision": "e4b1a5265da2cf19c08fff9c633870af",
+    "url": "/static/media/wrapper-background.e4b1a526.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06caa7b4a914be5ffbf4e3114f75dc3b.js"
+  "/precache-manifest.f5e8bd88b84aa56fb4095296d650cf7c.js"
 );
 
 self.addEventListener('message', (event) => {
